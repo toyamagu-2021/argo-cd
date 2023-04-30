9bda40fa812d3c432e9567a2cb66de15869072e1
tomoki-yamaguchi <toyamagu2021@gmail.com>
2023-05-01 08:38:34 +0900

    e2e
    Signed-off-by: tomoki-yamaguchi <toyamagu2021@gmail.com>

---
+++ test/e2e/app_skiphealthcheck_test.go
@@ -0,0 +1,26 @@
+package e2e
+
+import (
+	"testing"
+
+	"github.com/argoproj/argo-cd/v2/common"
+	. "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	. "github.com/argoproj/argo-cd/v2/test/e2e/fixture/app"
+	"github.com/argoproj/gitops-engine/pkg/health"
+)
+
+const (
+	failureManifestPath = "failure-during-sync"
+)
+
+func TestDontSkipHealthCheck(t *testing.T) {
+	Given(t).
+		Path(failureManifestPath).
+		When().
+		// app should have no status
+		CreateFromFile(func(app *Application) {
+			app.Annotations = map[string]string{common.AnnotationKeySkipHealthCheck: "not-skip"}
+		}).
+		Then().
+		Expect(HealthIs(health.HealthStatusHealthy))
+}
