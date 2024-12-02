30876257801c244eb9c6791f98348c197ab89e75
toyamagu-2021 <toyamagu2021@gmail.com>
2024-12-02 10:47:44 +0900

    fix: lint
    Signed-off-by: toyamagu-2021 <toyamagu2021@gmail.com>

--- reposerver/repository/repository_test.go
+++ reposerver/repository/repository_test.go
@@ -3100,7 +3100,8 @@ func TestFetchRevisionCanGetNonstandardRefs(t *testing.T) {
 	require.NoError(t, err)
 
 	// We should initialize repository
-	gitClient.Init()
+	err = gitClient.Init()
+	require.NoError(t, err)
 
 	pullSha, err := gitClient.LsRemote("refs/pull/123/head")
 	require.NoError(t, err)
